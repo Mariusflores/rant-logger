fa15374973f8ab9637cfa3e4a4675eec3bdffd8f
mariusflores <mfloresdal@gmail.com>
2026-05-02 11:24:17 +0200

    minimal working product

---
+++ src/main/java/io/rantlogger/RantLogger.java
@@ -0,0 +1,74 @@
+package io.rantlogger;
+
+import io.babyredis.client.BabyRedisClient;
+
+import java.util.Arrays;
+
+public class RantLogger {
+    public static final String RANT_SET = "rants";
+
+    public static void help() {
+        System.out.println("=== Rant Logger ===");
+        System.out.println("Usage: rant <message>        Log a rant");
+        System.out.println("       rant list             List all rants");
+        System.out.println("       rant find <id>        Find a specific rant");
+        System.out.println("       rant delete <id>      Delete a rant");
+        System.out.println("====================");
+        System.out.println("\r");
+        System.out.println("\r");
+    }
+
+    public static void main(String[] args) {
+        if(args.length == 0){
+            help();
+            return;
+        }
+
+        try (BabyRedisClient client = new BabyRedisClient("localhost", 6379)) {
+
+            String command = args[0].toUpperCase();
+
+            switch (command) {
+                case "LIST" -> {
+                    String[] rants = client.sMembers(RANT_SET);
+                    for(String key: rants){
+                        System.out.println(key + " → " + client.get(key));
+                    }
+                }
+                case "FIND" -> {
+                    if(args.length < 2){
+                        System.out.println("Usage: rant find <key>");
+                        return;
+                    }
+                    String key = args[1];
+                    String response = client.get(key);
+
+                    System.out.println(response);
+
+                }
+                case "DELETE" -> {
+                    if(args.length < 2){
+                        System.out.println("Usage: rant find <key>");
+                        return;
+                    }
+                    String key = args[1];
+                    client.sRem(RANT_SET, key);
+                    client.delete(key);
+
+                    System.out.println("Deleted.");
+
+                }
+                default -> {
+                    // everything else is a rant message
+                    String message = String.join(" ", args);
+
+                    String key = String.format("rant:%d", System.currentTimeMillis());
+                    // Add key to a set
+                    client.sAdd(RANT_SET, key);
+                    client.set(key, message);
+                    System.out.println("Rant logged: " + key);
+                }
+            }
+        }
+    }
+}
--- src/test/java/io/rantlogger/ExampleTest.java
+++ src/test/java/io/rantlogger/ExampleTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package io.rantlogger;
 
 import org.junit.jupiter.api.Test;
 
